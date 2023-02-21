15b838bd9792022909df6485d86d9bd82e35ddb8
soumayadip2002 <soumayadipsaha2002@gmail.com>
2023-02-21 21:53:12 +0530

    update

--- script.js
+++ script.js
@@ -117,3 +117,15 @@ var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
